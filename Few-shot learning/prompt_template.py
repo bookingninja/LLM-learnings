37ff3120c2395a9b01e42e11eb7e2e79c78bb2f4
Brent Ramirez <46661360+bookingninja@users.noreply.github.com>
2023-11-22 13:09:24 -0800

    Create prompt_template.py

---
+++ Few-shot learning/prompt_template.py
@@ -0,0 +1,13 @@
+# Sample prompt outline 
+
+prompt =\
+"""<High-level instruction about the task>:
+
+[<input_label>]: "<input text>"
+[<output_label>]: "<output_text>"
+###
+[<input_label>]: "<input text>"
+[<output_label>]: "<output_text>"
+###
+[<input_label>]: "<input text>"
+[<output_label>]:"""
